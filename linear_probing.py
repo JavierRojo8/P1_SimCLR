3a2d45aae4b966ea9cf9d8ac104021c277fde7ad
xAlexMGGx <alex.m.guinea@gmail.com>
2026-02-03 10:07:49 +0100

    [CODE] - Modularize the linear probing training

--- linear_probing.py
+++ linear_probing.py
@@ -1,89 +1,91 @@
-import argparse
+import logging
+import os
+import sys
+
 import torch
-import torch.backends.cudnn as cudnn
-from torchvision import models
-from data_aug.contrastive_learning_dataset import ContrastiveLearningDataset
-from models.resnet_simclr import ResNetSimCLR
-from simclr import SimCLR
+from datetime import datetime
+import torch.nn.functional as F
+from torch.amp import GradScaler, autocast
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+from utils import save_config_file, accuracy, save_checkpoint
 
-model_names = sorted(name for name in models.__dict__
-                     if name.islower() and not name.startswith("__")
-                     and callable(models.__dict__[name]))
-
-parser = argparse.ArgumentParser(description='PyTorch SimCLR')
-parser.add_argument('-data', metavar='DIR', default='./datasets',
-                    help='path to dataset')
-parser.add_argument('-dataset-name', default='stl10',
-                    help='dataset name', choices=['stl10', 'cifar10'])
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                         ' | '.join(model_names) +
-                         ' (default: resnet50)')
-parser.add_argument('-j', '--workers', default=12, type=int, metavar='N',
-                    help='number of data loading workers (default: 32)')
-parser.add_argument('--epochs', default=200, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
-                    metavar='N',
-                    help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--lr', '--learning-rate', default=0.0003, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)',
-                    dest='weight_decay')
-parser.add_argument('--seed', default=None, type=int,
-                    help='seed for initializing training. ')
-parser.add_argument('--disable-cuda', action='store_true',
-                    help='Disable CUDA')
-parser.add_argument('--fp16-precision', action='store_true',
-                    help='Whether or not to use 16-bit precision GPU training.')
-
-parser.add_argument('--out_dim', default=128, type=int,
-                    help='feature dimension (default: 128)')
-parser.add_argument('--log-every-n-steps', default=100, type=int,
-                    help='Log every n steps')
-parser.add_argument('--temperature', default=0.07, type=float,
-                    help='softmax temperature (default: 0.07)')
-parser.add_argument('--n-views', default=2, type=int, metavar='N',
-                    help='Number of views for contrastive learning training.')
-parser.add_argument('--gpu-index', default=0, type=int, help='Gpu index.')
+torch.manual_seed(0)
 
 
-def main():
-    args = parser.parse_args()
-    assert args.n_views == 2, "Only two view training is supported. Please use --n-views 2."
-    # check if gpu training is available
-    if not args.disable_cuda and torch.cuda.is_available():
-        args.device = torch.device('cuda')
-        cudnn.deterministic = True
-        cudnn.benchmark = True
-    else:
-        args.device = torch.device('cpu')
-        args.gpu_index = -1
+class LinearProbing(object):
 
-    dataset = ContrastiveLearningDataset(args.data)
+    def __init__(self, *args, **kwargs):
+        self.args = kwargs['args']
+        self.model = kwargs['model'].to(self.args.device)
+        self.optimizer = kwargs['optimizer']
+        self.scheduler = kwargs['scheduler']
+        self.writer = SummaryWriter()
+        logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
+        self.criterion = torch.nn.CrossEntropyLoss().to(self.args.device)
 
-    train_dataset = dataset.get_dataset(args.dataset_name, args.n_views)
+    def train(self, train_loader):
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True, drop_last=True)
+        scaler = GradScaler('cuda',enabled=self.args.fp16_precision)
 
-    model = ResNetSimCLR(base_model=args.arch, out_dim=args.out_dim)
+        # save config file
+        save_config_file(self.writer.log_dir, self.args)
 
-    optimizer = torch.optim.Adam(model.parameters(), args.lr, weight_decay=args.weight_decay)
+        n_iter = 0
+        logging.info(f"Start Linear_Probing training for {self.args.epochs} epochs.")
+        logging.info(f"Training with gpu: {self.args.disable_cuda}.")
 
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=0,
-                                                           last_epoch=-1)
+        for epoch_counter in range(self.args.epochs):
+            for images, _ in tqdm(train_loader):
+                images = torch.cat(images, dim=0)
 
-    #  It’s a no-op if the 'gpu_index' argument is a negative integer or None.
-    with torch.cuda.device(args.gpu_index):
-        simclr = SimCLR(model=model, optimizer=optimizer, scheduler=scheduler, args=args)
-        simclr.train(train_loader)
+                images = images.to(self.args.device)
+
+                with autocast('cuda', enabled=self.args.fp16_precision):
+                    features = self.model(images)
+                    logits, labels = self.info_nce_loss(features)
+                    loss = self.criterion(logits, labels)
+
+                self.optimizer.zero_grad()
+
+                scaler.scale(loss).backward()
+
+                scaler.step(self.optimizer)
+                scaler.update()
+
+                if n_iter % self.args.log_every_n_steps == 0:
+                    top1, top5 = accuracy(logits, labels, topk=(1, 5))
+                    self.writer.add_scalar('loss', loss, global_step=n_iter)
+                    self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
+                    self.writer.add_scalar('acc/top5', top5[0], global_step=n_iter)
+                    self.writer.add_scalar('learning_rate', self.scheduler.get_lr()[0], global_step=n_iter)
+
+                n_iter += 1
+
+            # warmup for the first 10 epochs
+            if epoch_counter >= 10:
+                self.scheduler.step()
+            logging.debug(f"Epoch: {epoch_counter}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
+            if epoch_counter % 30 == 0:
+                # save model checkpoints in a folder
+                mkdir_path = os.path.join(self.writer.log_dir, 'checkpoints')
+                os.makedirs(mkdir_path, exist_ok=True)
+                checkpoint_name = f'checkpoint_{datetime.timestamp}_{epoch_counter:04d}.pth.tar'
+                save_checkpoint({
+                    'epoch': epoch_counter,
+                    'arch': self.args.arch,
+                    'state_dict': self.model.state_dict(),
+                    'optimizer': self.optimizer.state_dict(),
+                }, is_best=False, filename=os.path.join(mkdir_path, checkpoint_name))
 
 
-if __name__ == "__main__":
-    main()
+        logging.info("Training has finished.")
+        # save model checkpoints
+        checkpoint_name = f'checkpoint_{datetime.timestamp}_{self.args.epochs:04d}.pth.tar'
+        save_checkpoint({
+            'epoch': self.args.epochs,
+            'arch': self.args.arch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
+        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
--- models/resnet_linear_probing.py
+++ models/resnet_linear_probing.py
@@ -4,10 +4,10 @@ import torchvision.models as models
 from exceptions.exceptions import InvalidBackboneError
 
 
-class ResNetSimCLR(nn.Module):
+class ResNetLP(nn.Module):
 
-    def __init__(self, base_model, out_dim):
-        super(ResNetSimCLR, self).__init__()
+    def __init__(self, base_model, out_dim, freeze_backbone=True):
+        super(ResNetLP, self).__init__()
         self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
                             "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)}
 
@@ -15,7 +15,13 @@ class ResNetSimCLR(nn.Module):
         dim_mlp = self.backbone.fc.in_features
 
         # add mlp projection head
-        self.backbone.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.fc)
+        self.backbone.fc = nn.Linear(dim_mlp, out_dim)
+        
+        if freeze_backbone:
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+            for param in self.backbone.fc.parameters():
+                param.requires_grad = True
 
     def _get_basemodel(self, model_name):
         try:
